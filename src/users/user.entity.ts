a86a5c198605a3a8bbc61530eb0d006f53622482
2chanwoo <navillela.808@gmail.com>
2024-02-12 19:15:07 +0900

    53~56. [TypeOrm] hooks  와  Save() vs. Update()

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()  //* 근데... service의 create() 직후 로그보다는 느리게 발생함.
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,10 +13,36 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    console.log('UsersService.create()');
+
     const user = this.repo.create({ email, password });
+    //Creates a new entity instance and copies all entity properties from this object into a new entity.
+    //Note that it copies only properties that are present in entity schema.
+    console.log('UsersService.create() - after creating');
     //* create 를 먼저 해 주는 이유는, Entity 객체로 말아 질 때, Validation을 위함.
     //? 그럼.. 여기서는 repo.create 를 했는데, 카카수 플젝에서는 Entity.create 를 하는데 뭐가 다른 걸까
-    return this.repo.save(user);
+    //? ㄴ> 메서드 주석 읽어보면, 똑같음 ㅎ
+    return this.repo.save(user); //Finds entities that match given find options.
     // return this.repo.create({ email, password });
+  }
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+    // Finds first entity that matches given where condition. If entity was not found in the database - returns null.
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+    //Finds entities that match given find options.
+  }
+
+  //! 56강. 강의에서는 hook때문에 DB를 두 번 호출하게 되는 find-save 메서드를 사용.
+  //! 카카수 플젝에서는 DB 한 번 호출하게 되는, update() 메서드를 사용!   ..... 훅 없이 log 로 출력하면 되니께.. update가 낫겄지?
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('uesr not found');
+    }
+    Object.assign(user, attrs); //* copyWith 같은 거.   ... 반환값이 있는데, 얕은 복사로 새 값 넣어져서 새 인스턴스로 만들지 않아도 되나보넹
+    return this.repo.save(user);
   }
 }
