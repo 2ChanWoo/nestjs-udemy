b0e193f5fdfcef915acd0708812735da94fb86f6
2chanwoo <navillela.808@gmail.com>
2024-03-05 00:44:22 +0900

    69. <?!> Type safety

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,7 +8,13 @@ import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export function Serialize(dto: any) {
+interface ClassConstructor {
+  //? new 키워드로 함수를 호출하면 함수가 생성자로 사용됩니다. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new)
+  // 이제 @Serialize(UserDto) 이렇게 선언할 때, 인스턴스만 가능해져서 타입세이프해짐?
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
