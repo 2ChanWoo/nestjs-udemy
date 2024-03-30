70b3fee2add22e97dd767c68c69bf508b0952c8d
2chanwoo <navillela.808@gmail.com>
2024-03-31 04:54:43 +0900

    76. Handling user signin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -29,6 +29,12 @@ export class UsersController {
     return this.authService.signup(body.email, body.password);
   }
 
+  @Post('/signin')
+  signin(@Body() body: CreateUestDto) {
+    console.log(body);
+    return this.authService.signin(body.email, body.password);
+  }
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('handler is running');
