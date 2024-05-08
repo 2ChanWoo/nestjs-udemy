086997c3ac9ef265b0995ab81eca2dabe953c969
2chanwoo <navillela.808@gmail.com>
2024-05-08 23:55:38 +0900

    109. Not super effective test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,7 +3,10 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
+//* 컨트롤러는 대체로 단순하고, 단순해야 하기 때문에 테스트할 게 별로 없다고 함.(find~ 같은거..)
+//* signin은 테스트 할만 하다고 함.
 describe('UsersController', () => {
   let controller: UsersController;
   let fakeUsersService: Partial<UsersService>;
@@ -47,5 +50,25 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUesrs returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    const user = controller.findUser('1');
+    expect(user).rejects.toThrow(NotFoundException);
+    //? 내가한거.. it의 콜백함수에서는 에러가 안터지나?
+    //? 왜 에러가 안터지지?
+    // expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 });
