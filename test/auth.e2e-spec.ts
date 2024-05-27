884e134f9a6ab9608a9ae695e37e5f2a072bc453
2chanwoo <navillela.808@gmail.com>
2024-05-28 00:30:39 +0900

    comment

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,5 +27,8 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
         expect(password).toBeUndefined();
       });
+    //* 위처럼 메서드 체이닝에서 오류가 나면 정확한 오류가 발견이 되지 않는건가?
+    //* 로그 출력해보려고 메서드 하나씩 변수 할당하여 호출하려고 코드 수정하니,
+    //* 터미널에서 오류 문구도 제대로 나왔다.
   });
 });
