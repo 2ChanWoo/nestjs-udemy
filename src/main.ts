0315a81b69a5ced0e952832a0c168a3bab21c3bf
2chanwoo <navillela.808@gmail.com>
2024-06-22 17:45:24 +0900

    123. !! ERROR !! connect ECONNREFUSED 127.0.0.1:80

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //? 강의자료에는 아래 코드가 추가되어있는데 뭐지?
+  (app as any).set('etag', false); // => 캐싱을 비활성화 하는 옵션?
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by'); //* => from ChatGPT: 이 헤더는 서버가 어떤 기술로 구동되고 있는지 정보를 제공합니다. 보안상의 이유로 제거하는 것이 좋습니다.
+    res.removeHeader('date'); //* => from ChatGPT: 이 헤더는 응답이 생성된 시간을 포함합니다. 특정 보안 요구 사항이나 성능 최적화를 위해 제거할 수 있습니다.
+    /**
+     *! 위 추가설정처럼 설정하면 좋은 것들.
+     * ? - Helmet: 여러 보안 헤더를 설정해주는 미들웨어
+     * ? - CORS 설정: 외부 도메인 요청 제어
+     * ? - Rate Limiting: 요청 속도를 제한하여 DOS 공격을 방지합니다. (npm: express-rate-limit)
+     *
+     * ++ 성능 최적화
+     * - Compression: 응답 데이터를 압축하여 네트워크 성능을 향상시킵니다.
+     * - Cache Control: 정적 파일이나 API응답에 대한 캐싱을 설정합니다.
+     *
+     */
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -31,4 +31,27 @@ describe('Authentication System', () => {
     //* 로그 출력해보려고 메서드 하나씩 변수 할당하여 호출하려고 코드 수정하니,
     //* 터미널에서 오류 문구도 제대로 나왔다.
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    //* signup request는 쿠키로 응답하기 때문에, 쿠키를 받을 객체가 필요
+    //* test 에서 cookie는 지원하지 않기 떄문에, 변수에 할당하여 직접 꺼내야 함.
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+    console.log('test test test');
+    // console.log(res);
+    console.log(cookie);
+
+    const { body } = await request(app.getHttpServer())
+      .get('auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
