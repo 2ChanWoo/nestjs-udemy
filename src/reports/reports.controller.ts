e85b09a4a37f5ae0824f4d9951aad33e2166d8d4
2chanwoo <navillela.808@gmail.com>
2024-06-29 21:15:41 +0900

    135. 코드로 Report 생성시 User 대입하기. (id가 아닌 User 인스턴스 통으로)

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorators';
+import { User } from '../users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,9 +11,9 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     console.log('createReport()');
 
-    return this.reportService.create(body);
+    return this.reportService.create(body, user);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,14 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportDto: CreateReportDto) {
+  create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
-
+    report.user = user; //* user.id 만 넣는게 아님. TypeORM 에서 설정한대로 User인스턴스에서 id만 추출해 넣음.
     return this.repo.save(report);
   }
 }
