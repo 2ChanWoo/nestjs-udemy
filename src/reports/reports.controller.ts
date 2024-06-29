a0f2385308d035483544f2916d356e6ff1fb16eb
2chanwoo <navillela.808@gmail.com>
2024-06-29 19:36:43 +0900

    ~129. Report Creation

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {
+    console.log('createReport()');
+
+    return this.reportService.create(body);
+  }
+}
