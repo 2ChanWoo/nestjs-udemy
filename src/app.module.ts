0536dbc08859886f29d3cf6a5733d1ccb45cb12b
2chanwoo <navillela.808@gmail.com>
2024-12-03 05:20:34 +0900

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -56,6 +56,7 @@ const cookieSession = require('cookie-session');
         whitelist: true,
         //! ㄴ> Validation 에 포함되지 않는 속성은 자동으로 제거됨. .... 난 false 하는게 좋을 것 같은데.. 보안문제로 true 하는게 좋다고 함?!
         //?   ㄴ> (ex, User {email, password} 라고 할 때, body에 {email, password, something} 이렇게 들어오면 controller 에서 body를 찍어봐도 email과 password 밖에 출력되지 않는다.)
+        //? true가 보안상 좋음. ++ 별매 ++ forbidNonWhitelisted: true 옵션과 함께 사용하면, 정의되지 않은 속성이 들어온 경우 요청을 거부합니다. 예를 들어, 위 예시에서 role 속성이 포함된 요청을 아예 400 Bad Request로 응답하게 할 수 있습니다.
       }),
     },
   ],
